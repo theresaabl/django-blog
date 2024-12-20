3aa2d4d85299a327d16df39c7fc8ca75bb3e44de
Theresa Abl <theresaabl@gmx.at>
2024-12-20 16:35:22 +0000

    Modify standard admin dashboard and integrate summernote package

--- codestar/urls.py
+++ codestar/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# order string paths alphabetically with the empty string path last
 urlpatterns = [
-    path('', include("blog.urls"), name='blog-urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('', include("blog.urls"), name='blog-urls'),
 ]
