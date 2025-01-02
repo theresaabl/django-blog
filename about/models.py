7e513e8eeec6bd376c352a1010fa7159acf9205b
Theresa Abl <theresaabl@gmx.at>
2025-01-02 12:19:21 +0000

    Create about view

--- about/models.py
+++ about/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class About(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
--- about/views.py
+++ about/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import About
+
 
 # Create your views here.
+def about(request):
+
+    queryset = About.objects.all().order_by("-updated_on")
+    about = get_object_or_404(queryset.first())
+
+    return render(
+        request,
+        "about/about.html",
+        {
+            "about": about,
+        },
+    )
