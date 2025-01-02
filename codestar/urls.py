caa11a71453e7fe415b7bdb9fee2122a2a6789f1
Theresa Abl <theresaabl@gmx.at>
2025-01-02 15:32:56 +0000

    Add authentication

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 # order string paths alphabetically with the empty string path last
 urlpatterns = [
     path('about/', include("about.urls"), name='about-urls'),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include("blog.urls"), name='blog-urls'),
