c610091ed72f548986db5675cc4816b51fbd3745
Theresa Abl <theresaabl@gmx.at>
2025-01-02 12:27:34 +0000

    Create about template and urls file

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 # order string paths alphabetically with the empty string path last
 urlpatterns = [
+    path('about/', include("about.urls"), name='about-urls'),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include("blog.urls"), name='blog-urls'),
-    # path('about/', include("about.urls"), name='about-urls'),
 ]
