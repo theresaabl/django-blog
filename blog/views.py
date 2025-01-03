17320a80cbc3d30cd15a3e6352525205e522a9a3
Theresa Abl <theresaabl@gmx.at>
2025-01-03 18:07:45 +0000

    Fix typo in blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -63,14 +63,12 @@ def comment_edit(request, slug, comment_id):
     view to edit comments
     """
     if request.method == "POST":
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comment = get_object_or_404(Comment, pk=comment_id)
+        comment = get_object_or_404(Comment, pk=comment_id)  # pk - primary key
         comment_form = CommentForm(data=request.POST, instance=comment)
 
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
-            comment.post = post
+            print(comment.post)
             comment.approved = False
             comment.save()
             messages.add_message(request, messages.SUCCESS, 'Comment updated!')
